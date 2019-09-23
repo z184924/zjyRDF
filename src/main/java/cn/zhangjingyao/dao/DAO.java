17d9fe919aee85cce8b5c23d2866529c26eed79b
z184924 <z184924@126.com>
2019-09-23 18:05:26 +0800

    解决无需抛出异常的时候却有异常抛出声明

--- src/main/java/cn/zhangjingyao/dao/DAO.java
+++ src/main/java/cn/zhangjingyao/dao/DAO.java
@@ -12,45 +12,40 @@ public interface DAO {
 	 * @param str Mapper名及sql名 eg: DemoMapper.save
 	 * @param obj 传入sql的对象
 	 * @return sql执行后的返回对象
-	 * @throws Exception Exception
 	 */
-	public Object save(String str, Object obj) throws Exception;
+	public Object save(String str, Object obj);
 	
 	/**
 	 * 修改对象
 	 * @param str Mapper名及sql名 eg: DemoMapper.update
 	 * @param obj 传入sql的对象
 	 * @return sql执行后的返回对象
-	 * @throws Exception Exception
 	 */
-	public Object update(String str, Object obj) throws Exception;
+	public Object update(String str, Object obj);
 	
 	/**
 	 * 删除对象 
 	 * @param str Mapper名及sql名 eg: DemoMapper.delete
 	 * @param obj 传入sql的对象
 	 * @return sql执行后的返回对象
-	 * @throws Exception Exception
 	 */
-	public Object delete(String str, Object obj) throws Exception;
+	public Object delete(String str, Object obj);
 
 	/**
 	 * 查找对象(单行结果)
 	 * @param str Mapper名及sql名 eg: DemoMapper.findById
 	 * @param obj 传入sql的对象
 	 * @return sql执行后的结果
-	 * @throws Exception Exception
 	 */
-	public Object findForObject(String str, Object obj) throws Exception;
+	public Object findForObject(String str, Object obj);
 
 	/**
 	 * 查找对象(多行结果)
 	 * @param str Mapper名及sql名 eg: DemoMapper.findByName
 	 * @param obj 传入sql的对象
 	 * @return sql执行后的结果
-	 * @throws Exception Exception
 	 */
-	public Object findForList(String str, Object obj) throws Exception;
+	public Object findForList(String str, Object obj);
 
 	/**
 	 * 查找对象封装成Map
@@ -58,16 +53,14 @@ public interface DAO {
 	 * @param obj 传入sql的对象
 	 * @param key mapKey
 	 * @return 查询结果Map
-	 * @throws Exception Exception
 	 */
-	public Object findForMap(String sql, Object obj, String key) throws Exception;
+	public Object findForMap(String sql, Object obj, String key);
 
 	/**
 	 * 整合新增
 	 * @param paramString 参数名
 	 * @param paramList	参数列表
-	 * @throws Exception Exception
 	 */
-	public abstract void batchInsert(String paramString, List paramList)throws Exception;
+	public abstract void batchInsert(String paramString, List paramList);
 	
 }
